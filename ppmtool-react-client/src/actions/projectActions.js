eae6ea5eee367c092afc8b267b5e69fdf66afde2
Mandar Palekar <mandarpalekar@gmnail.com>
2019-11-21 07:05:25 +1030

    created updateProject component and mapped state to props

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,22 +1,33 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS} from "./types";
+import { GET_ERRORS, GET_PROJECT, GET_PROJECTS } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
-    try{
-        const res = await axios.post("http://localhost:8080/api/project/create", project);
-        history.push("/dashboard");
-    } catch (error) {
-        dispatch({
-            type:GET_ERRORS,
-            payload:error.response.data
-        });
-    }
+  try {
+    const res = await axios.post(
+      "http://localhost:8080/api/project/create",
+      project
+    );
+    history.push("/dashboard");
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data
+    });
+  }
 };
 
 export const getProjects = () => async dispatch => {
-    const  res = await axios.get("http://localhost:8080/api/project/read");
-    dispatch({
-        type:GET_PROJECTS,
-        payload:res.data
-    });
+  const res = await axios.get("http://localhost:8080/api/project/read");
+  dispatch({
+    type: GET_PROJECTS,
+    payload: res.data
+  });
+};
+
+export const getProject = (id, history) => async dispatch => {
+  const res = await axios.get(`http://localhost:8080/api/project/read/${id}`);
+  dispatch({
+    type: GET_PROJECT,
+    payload: res.data
+  });
 };
--- ppmtool-react-client/src/reducers/projectReducer.js
+++ ppmtool-react-client/src/reducers/projectReducer.js
@@ -1,19 +1,24 @@
-import { GET_PROJECTS } from "../actions/types";
+import { GET_PROJECT, GET_PROJECTS } from "../actions/types";
 
-const  initialState = {
-    projects: [],
-    project: {}
+const initialState = {
+  projects: [],
+  project: {}
 };
 
-export default function (state = initialState, action) {
-    switch (action.type) {
-        case GET_PROJECTS:
-            return {
-                ...state,
-                projects: action.payload
-            };
-        default:
-            return state;
-    }
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_PROJECTS:
+      return {
+        ...state,
+        projects: action.payload
+      };
+    case GET_PROJECT:
+      return {
+        ...state,
+        project: action.payload
+      };
+    default:
+      return state;
+  }
 }
 
